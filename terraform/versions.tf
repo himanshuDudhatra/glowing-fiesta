ef8be76c1cc844dd5beef2ec1a6ee8d180f7594d
Himanshu Dudhatra <himanshududhatra123@gmail.com>
2026-04-09 00:37:44 +0530

    Update Terraform configuration to replace sensitive backend details with placeholders

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,31 +1,12 @@
-# terraform {
-#   required_version = "1.14.8"
-
-#   backend "s3" {
-#     bucket       = "REPLACE_WITH_TF_STATE_BUCKET"
-#     key          = "REPLACE_WITH_STATE_KEY"
-#     region       = "REPLACE_WITH_AWS_REGION"
-#     encrypt      = true
-#     use_lockfile = true
-#   }
-
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "6.39.0"
-#     }
-#   }
-# }
-
 terraform {
   required_version = "1.14.8"
 
   backend "s3" {
-    bucket         = "utopikai-infra-dev-tf"
-    key            = "test.tfstate"
-    region         = "eu-west-1"
-    encrypt        = true
-    use_lockfile   = true
+    bucket       = "REPLACE_WITH_TF_STATE_BUCKET"
+    key          = "REPLACE_WITH_STATE_KEY"
+    region       = "REPLACE_WITH_AWS_REGION"
+    encrypt      = true
+    use_lockfile = true
   }
 
   required_providers {
